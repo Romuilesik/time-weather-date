95250a2b6add99bf8010febac5b3e03c645bc36d
Romuilesik <roma.ilesik@gmail.com>
2023-03-04 10:35:40 +0200

    omg

---
+++ main.py
@@ -0,0 +1,59 @@
+import requests
+import sqlite3
+from bs4 import BeautifulSoup
+import datetime
+
+conn = sqlite3.connect('weather.db')
+
+c = conn.cursor()
+
+
+c.execute('''CREATE TABLE IF NOT EXISTS weather
+
+            (date_time TEXT, temperature REAL)''')
+
+
+conn.commit()
+
+conn.close()
+
+url = 'https://ua.sinoptik.ua/погода-львів'
+
+response = requests.get(url)
+
+
+if response.status_code == 200:
+
+   html = response.content
+
+else:
+
+   print('Не вдалося отримати сторінку')
+soup = BeautifulSoup(html, 'html.parser')
+
+today_weather = soup.find('div', {'class': 'weatherToday'}).find('div', {'class': 'temperature'}).text
+
+today_temperature = int(today_weather.split('°')[0])
+
+
+print(f"Температура сьогодні: {today_temperature}°C")
+
+temperature = today_temperature
+
+date_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+conn = sqlite3.connect('weather.db')
+
+c = conn.cursor()
+
+
+c.execute("INSERT INTO weather VALUES (?, ?)", (date_time, temperature))
+
+
+conn.commit()
+
+conn.close()
+
+
+
